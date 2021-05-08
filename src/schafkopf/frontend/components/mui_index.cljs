5ffe54b6c79397f91bcef27e60b5a8657551c66e
Ferdinand Beyer <fb@fbeyer.com>
2021-05-08 17:28:12 +0200

    Strip unused MUI modules

--- src/schafkopf/frontend/components/mui_index.cljs
+++
@@ -1,5 +0,0 @@
-(ns schafkopf.frontend.components.mui-index
-  "Requires other namespaces that use Material UI components."
-  (:require [mui-bien.core.all] ;; TODO: Remove
-            schafkopf.frontend.components.hand
-            schafkopf.frontend.components.playing-card))
